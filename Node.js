d38d8e41d80afb738890661001c39e9d9c8e73d1
Ubuntu <ubuntu@ip-172-31-88-158.ec2.internal>
2020-05-15 16:54:49 +0000

    its not that hard

---
+++ Node.js
@@ -0,0 +1,10 @@
+// JavaScript File
+Name: <input type="text" id="myText" value="Mickey">
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  document.getElementById("myText").value = "Johnny Bravo";
+}
+</script>
